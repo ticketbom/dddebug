357b06b2d8f48b45318a719b4d77ab4d961ccc00
Guilherme Lima <ticketbom.tb@gmail.com>
2024-05-19 00:42:15 -0300

    commit icial v0

---
+++ debug.go
@@ -0,0 +1,23 @@
+package mydebug
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+// DD dumps the given data and terminates the program.
+func DD(data interface{}) {
+	// Convert the data to a pretty-printed JSON string for readability
+	bytes, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal data: %v", err)
+	}
+
+	// Print the pretty-printed JSON string
+	fmt.Println(string(bytes))
+
+	// Exit the program
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/seuusuario/mydebug
+
+go 1.21.1
